eabec24e25c6eba504900576e2cb0504e6fa7fc8
Ajay Kumar Saroj <ajaykumarsaroj13@gmail.com>
2026-06-21 12:19:11 +0530

    Fix cache issue and correctOptionsArray

--- check_ids.py
+++
@@ -1,6 +0,0 @@
-import json
-data = json.load(open('blackBookDataFull.json', 'r', encoding='utf-8'))
-for ch in data:
-    title = ch.get('title', '')
-    if any(n in title.lower() for n in ['indefinite', 'area', 'differential']):
-        print(f"Chapter: {title}, ID: {ch['id']}")
